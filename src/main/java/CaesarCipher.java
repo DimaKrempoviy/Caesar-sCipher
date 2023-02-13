314e45480878a8b7154683bc3b90944ed744601e
DimaKrempoviy <krempoviy98@gmail.com>
2023-02-13 18:52:14 +0200

    Refactor patch

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,38 +1,51 @@
 import java.util.Scanner;
 
-public class CaesarCipher {
+class CaesarCipher {
 
-    public static String encrypt(String plainText, int shift) {
-        String encryptedText = "";
-        for (int i = 0; i < plainText.length(); i++) {
-            char c = plainText.charAt(i);
-            if (Character.isLetter(c)) {
-                c = (char) (c + shift);
-                if (Character.isUpperCase(plainText.charAt(i)) && c > 'Z'
-                        || Character.isLowerCase(plainText.charAt(i)) && c > 'z') {
-                    c = (char) (c - 26);
-                }
-            }
-            encryptedText += c;
-        }
-        return encryptedText;
-    }
-
-    public static String decrypt(String encryptedText, int shift) {
-        return encrypt(encryptedText, 26 - shift);
-    }
+    private static final String PLAINT_TEXT_REQUEST = "Enter the plain text: ";
+    private static final String SHIFT_REQUEST = "Enter the shift value: ";
+    private static final String ENCRYPTED_TEXT_OUTPUT_PREFIX = "Encrypted Text: ";
+    private static final String DECRYPTED_TEXT_OUTPUT_PREFIX = "Decrypted Text: ";
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the plain text: ");
-        String plainText = sc.nextLine();
-        System.out.println("Enter the shift value: ");
-        int shift = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(PLAINT_TEXT_REQUEST);
+        String plainText = scanner.nextLine();
+
+        System.out.println(SHIFT_REQUEST);
+        int shift = scanner.nextInt();
+
         String encryptedText = encrypt(plainText, shift);
-        System.out.println("Encrypted Text: " + encryptedText);
+        System.out.println(ENCRYPTED_TEXT_OUTPUT_PREFIX + encryptedText);
+
         String decryptedText = decrypt(encryptedText, shift);
-        System.out.println("Decrypted Text: " + decryptedText);
-        sc.close();
+        System.out.println(DECRYPTED_TEXT_OUTPUT_PREFIX + decryptedText);
+
+        scanner.close();
+    }
+
+    private static String encrypt(String plainText, int shift) {
+        StringBuilder encryptedText = new StringBuilder();
+
+        for (int i = 0; i < plainText.length(); i++) {
+            char character = plainText.charAt(i);
+
+            if (Character.isLetter(character)) {
+                character = (char) (character + shift);
+
+                if (Character.isUpperCase(plainText.charAt(i)) && character > 'Z'
+                        || Character.isLowerCase(plainText.charAt(i)) && character > 'z') {
+                    character = (char) (character - 26);
+                }
+            }
+            encryptedText.append(character);
+        }
+        return encryptedText.toString();
+    }
+
+    private static String decrypt(String encryptedText, int shift) {
+        return encrypt(encryptedText, 26 - shift);
     }
 
 }
